387581686a24633c94c2aaae7230aa1bd2ed07ba
Daniel Traian Pele <danpele@ase.ro>
2020-02-20 10:59:14 +0200

    Add files via upload

--- TSD/time_series_decomposition.R
+++ TSD/time_series_decomposition.R
@@ -41,13 +41,15 @@ y
 
 #plot the time series
 plot.ts(y)
-x11()
 
+
+#install.packages("TTR")
+library(TTR)
 
 #seasonal moving average 12
 dataSMA12 <- SMA(y,n=12)
 plot.ts(dataSMA12)
-x11()
+
 
 
 #time series decomposition
@@ -57,7 +59,7 @@ datacomponents
 
 #plot the components
 plot(datacomponents)
-x11()
+
 
 #time series decomposition
 datacomponents <- decompose(y, "additive")
@@ -66,4 +68,3 @@ datacomponents
 
 #plot the components
 plot(datacomponents)
-x11()
